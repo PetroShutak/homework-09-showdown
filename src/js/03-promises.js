708a5c7a1781aa61c44502e178beba807f58a4d0
Petro Shutak <113899587+PetroShutak@users.noreply.github.com>
2023-04-07 11:55:53 +0300

    start showdown task-02
    add library

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,49 @@
+// const fetchUserFromServer = (username, onSuccess, onError) => {
+//     console.log(`Fetching data for ${username}`);
+  
+//     setTimeout(() => {
+//       // Change value of isSuccess variable to simulate request status
+//       const isSuccess = true;
+  
+//       if (isSuccess) {
+//         onSuccess("success value");
+//       } else {
+//         onError("error");
+//       }
+//     }, 2000);
+//   };
+  
+//   const onFetchSuccess = user => {
+//     console.log(user);
+//   };
+  
+//   const onFetchError = error => {
+//     console.error(error);
+//   };
+  
+  // fetchUserFromServer("Mango", onFetchSuccess, onFetchError);
+
+  // const fetchUserFromServer = username => {
+  //   return new Promise((resolve, reject) => {
+  //     console.log(`Fetching data for ${username}`);
+  
+  //     setTimeout(() => {
+  //       // Change value of isSuccess variable to simulate request status
+  //       const isSuccess = true;
+  
+  //       if (isSuccess) {
+  //         resolve("success value");
+  //       } else {
+  //         reject("error");
+  //       }
+  //     }, 2000);
+  //   });
+  // };
+  
+  // fetchUserFromServer("Mango")
+  //   .then(user => console.log(user))
+  //   .catch(error => console.error(error));
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
